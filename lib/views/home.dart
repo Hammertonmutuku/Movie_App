c1efcb1672f3a4068b5316e38de43f70b16b8c6f
Hammertonmutuku <lthamma3@gmail.com>
2022-04-27 13:59:03 +0300

    added the movie model

---
+++ lib/views/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyhomePage extends StatefulWidget {
+  const MyhomePage({ Key? key }) : super(key: key);
+
+  @override
+  State<MyhomePage> createState() => _MyhomePageState();
+}
+
+class _MyhomePageState extends State<MyhomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
